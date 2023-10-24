f970ee8ded3a52d2d366a297c36220628909af34
Jiwon <kimg1623@naver.com>
2023-10-24 16:01:05 +0900

    :sparkles: Feat: 헤더 javascript 애니메이션 추가

--- portfolio-project/script.js
+++ portfolio-project/script.js
@@ -32,3 +32,35 @@
 	}
 	writeTxt(); // index 값을 모두 돌고 나면 index가 0인 상태로 다시 실행
 })();
+
+/****** 헤더 영역 스크롤 이벤트 추가 ******/
+// 수직 스크롤이 발생하면 header 영역에 active 클래스 추가
+const headerEl = document.querySelector("header");	// header영역 요소 가져오기
+window.addEventListener('scroll', function(){	// 스크롤 이벤트가 발생하면
+	this.requestAnimationFrame(scrollCheck);		// 
+});
+
+function scrollCheck(){
+	let browserScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+	if(browserScrollY > 0){
+		headerEl.classList.add("active");
+	} else {
+		headerEl.classList.remove("active");
+	}
+}
+
+/****** 헤더영역의 메뉴를 클릭했을 때 이동 애니메이션 구현 ******/
+const animationMove = function(selector){
+	const targetEl = document.querySelector(selector);	// selector 매개변수로 이동할 대상 요소 노드 가져오기
+	const browserScrollY = window.pageYOffset; // 현재 웹 브라우저의 스크롤 정보 (y값)
+	const targetScrollY = targetEl.getBoundingClientRect().top + browserScrollY; // 이동할 대상의 위치(y값)
+	window.scrollTo({top:targetScrollY, behavior: 'smooth'}); // 창 스크롤을 지정한 위치까지 부드럽게 이동
+};
+
+const scrollMoveEl = document.querySelectorAll("[data-animation-scroll='true']");	// 스크롤할 메뉴 요소 선택
+for(let i=0; i<scrollMoveEl.length; i++){	// 요소 길이만큼 반복
+	scrollMoveEl[i].addEventListener('click',function(e){	// 메뉴 요소에 클릭 이벤트 발생
+		const target = this.dataset.target;	// target 속성을 가져와서
+		animationMove(target);	// 해당 target으로 이동하는 애니메이션 함수 실행
+	});
+}
